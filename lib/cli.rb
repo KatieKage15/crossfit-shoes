c7e12eff8226527976f7b2e6126c0b60c36d023a
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-05 00:31:42 +0000

    undefined css method?!

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,12 +8,11 @@ def run
   puts "Which shoe would you like to choose?"
   html = open("https://www.store.crossfit.com/women-shoes")
   doc = Nokogiri::HTML(html)
-  doc.css(".partner").css(".filtervalue") #loop - run through shoes and list them
+  doc.css(".partner")[0].collect {|title|} #loop - run through shoes and list them
     title = shoes.css(".title").text.strip
-
-      binding.pry
     shoes << title
 
+      binding.pry
   shoes.each.with_index(1) do |title, i|
     puts "#{i}. #{title}"
   end
