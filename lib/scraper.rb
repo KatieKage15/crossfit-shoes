4c74c32e6049bb6b2ec12cf1909afedb8981a2ef
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-07 19:11:28 +0000

    figured out 2nd level scrape!

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,24 +2,22 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 class Scraper
+  @crossfit_shoe = "https://www.store.crossfit.com/women-shoes"
 
   def self.scrape_shoes
-    crossfit_shoe = "https://www.store.crossfit.com/women-shoes"
-    html = open(crossfit_shoe)
+    html = open(@crossfit_shoe)
     doc = Nokogiri::HTML(html)
     doc.css(".product-tile").each do |shoe| #loop - run through shoes and list them
       shoe_name = shoe.css(".title").text.strip
-      url = doc.css(".product-info-inner-content a").attr("href").value
+      url = doc.css(".hockeycard").attr("data-context").value
       shoe = Shoe.new(shoe_name, url)
       shoe.save
     end
   end
 
   def self.scrape_info(shoe)
-    crossfit_shoe = "https://www.store.crossfit.com/women-shoes"
-    html = open(crossfit_shoe + shoe.url)
+    html = open(@crossfit_shoe + shoe.url)
     doc = Nokogiri::HTML(html)
-      binding.pry 
   end
 
 
