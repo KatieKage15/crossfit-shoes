a9f2fc0d624b165c4bba222fb647a831ed6638a4
victorious-messaging-4309 <wildayk@gmail.com>
2019-04-08 11:52:16 +0000

    CLI project has color!

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,8 +3,8 @@ require 'pry'
 class Cli
 
   def run
-    puts "Welcome to the CrossFit Store!"
-    puts "Here are all our shoes:"
+    puts "Welcome to the CrossFit Store!".magenta
+    puts "Here are all our shoes:".magenta
     Scraper.scrape_shoes
     display_shoes
     menu
@@ -12,24 +12,26 @@ class Cli
 
   def display_shoes
     Shoe.all.each.with_index(1) do |shoe_name, i|
-      puts "#{i}. #{shoe_name.title}"
+      puts "#{i}. #{shoe_name.title}".magenta
     end
   end
 
   def menu
-    puts "What shoe would you like more information on?"
-    input = gets.chomp
-    shoe = Shoe.all[input.to_i - 1]
+    puts "What shoe would you like more information on?".white
+      input = gets.chomp
 
-    if !shoe
-      puts "Sorry, that isn't a correct entry."
-      puts "Please enter a valid shoe number."
-      menu
-    else
-      Scraper.scrape_info(shoe)
-      puts "Here are the details for #{shoe.title}:"
-      puts "The price is #{shoe.shoe_price}."
-      puts "#{shoe.shoe_product_details}"
-    end
+      shoe = Shoe.all[input.to_i - 1]
+
+      if !shoe
+        puts "Sorry, that isn't a correct entry.".red
+        puts "Please enter a valid shoe number.".red
+        menu
+      else
+        Scraper.scrape_info(shoe)
+        puts "Here are the details for #{shoe.title}:".blue
+        puts "The price is #{shoe.shoe_price}.".blue
+        puts "#{shoe.shoe_product_details}".blue
+        menu
+      end
   end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,8 @@
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'colorize'
+
 class Scraper
   @crossfit_shoe = "https://www.store.crossfit.com/women-shoes"
 
@@ -18,7 +20,7 @@ class Scraper
   def self.scrape_info(shoe)
     html = open(@crossfit_shoe + shoe.data_url)
     doc = Nokogiri::HTML(html)
-      shoe.shoe_price = doc.css(".buy-block-header").text.split[11..12].map { |a| "#{a}" }.join(" ")
+      shoe.shoe_price = doc.css(".price-in")[0].text.split.map { |a| "#{a}" }.join(" ")
       shoe.shoe_product_details = doc.css(".prod-details").text.strip
   end
 end
